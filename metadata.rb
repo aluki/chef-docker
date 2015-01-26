915ea88bf84e38dd9547b692b24933c33acc39e7
Alberto Tablado <alberto@aluki.org>
2015-01-26 15:44:22 +0000

    Fix README file name

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer 'Alberto Tablado'
 maintainer_email 'alberto@aluki.org'
 license 'gplv3'
 description 'Installs/Configures docker'
-long_description IO.read File.join File.dirname(__FILE__), 'README.rdoc'
+long_description IO.read File.join File.dirname(__FILE__), 'README.md'
 version '0.1.0'
 
